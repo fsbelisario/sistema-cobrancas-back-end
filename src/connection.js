23b2577a75dd12a53cad8174f20bcd3f5e940cb1
Felipe Belisário <fbelisario@gmail.com>
2021-10-02 14:57:07 -0300

    COmplemento do arquivo connection.js

--- src/connection.js
+++ src/connection.js
@@ -4,7 +4,11 @@ const knex = require('knex')({
     host: process.env.BD_HOST,
     user: process.env.BD_USER,
     password: process.env.BD_PASSWORD,
-    database: process.env.BD_DATABASE
+    database: process.env.BD_DATABASE,
+    port: process.env.BD_PORT,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
